dc0953bc582210ce52e287f610e8ba709f60d425
abhayull2008 <75789414+abhayull2008@users.noreply.github.com>
2021-04-22 19:39:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
 
 
 function draw() {
-  // background();
+  background("black");
    if(hasCollided(bullet,wall)){
    bullet.velocityX = 0;
    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
